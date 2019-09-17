ea399a287fb3ca089ebdabb5bd9b4028bbd84e4b
Laura Perez <laura.perez.garcia@physics.gu.se>
2019-09-17 11:24:45 +0200

    equipotential, MSD and potential analyisis

--- pot_analyisis_v1.m
+++ pot_analyisis_v1.m
@@ -25,7 +25,6 @@ disp(['dt: ' num2str(dt*subs)])
 [xbins,mU,EU,k_pot,Ek_pot,mhist,Ehist,h0,x_eq]=pot_nlfit_v1(x(1:subs:size(x,1),1),T,50);
 
 
-disp(['tau_0: ' num2str(6*pi*eta*a/k_pot)])
-disp(['dt: ' num2str(dt*subs)])
+
 
 
