19b94fa2717deaef684626edf118fbb640da7388
Laura Perez <laura.perez.garcia@physics.gu.se>
2019-11-06 11:08:41 +0100

    trials with psd

--- psd_analysis_prueba.m
+++ psd_analysis_prueba.m
@@ -23,13 +23,13 @@ kb=1.38064852e-23;
 D0=kb*T/gamma;
 
 subs=1; %use a subsampled data set
-
+for jj=1:5
 nw=round(size(x(1:subs:end,:),1)/500); % number of windows
 %[fc_psd,D_psd,sigma_fc_psd,sigma_D_psd,f,XX,fw_mean,Pk,fcut,h]=psdfit_analytic(x(1:subs:end,:),dt*subs,nw,1/4);
-[fc_psd,D_psd,sigma_fc_psd,sigma_D_psd,f,XX,fw_mean,Pk,sigma_Pk,fcut]=psd_lfit(x(1:subs:floor(end/5),:),dt*subs,nw,1/4);
+[fc_psd,D_psd,sigma_fc_psd,sigma_D_psd,f,XX,fw_mean,Pk,sigma_Pk,fcut]=psd_lfit(x(1:subs:end,jj),dt*subs,nw,1/4);
+Djj(jj)=mean(D_psd);
 
-
-gamma_psd=kb*T./mean(D_psd);
+gamma_psd(jj)=kb*T./mean(D_psd);
 
 sigma_gamma_psd=kb*T./D_psd^2*sigma_D_psd;
 
@@ -37,7 +37,7 @@ sigma_gamma_psd=kb*T./D_psd^2*sigma_D_psd;
 sigma_k_psd=2*pi*gamma*sigma_fc_psd;
 
 % estimation of k using the estimated gamma
-k_psd=2*pi*gamma_psd.*fc_psd;
+k_psd(jj)=2*pi*gamma_psd(jj).*fc_psd;
 
 sigma_k_psd=2*pi*gamma_psd.*sigma_fc_psd+2*pi*fc_psd*sigma_gamma_psd;
 
@@ -50,4 +50,14 @@ disp(['gamma: ' num2str(gamma_psd*1e9) '+-' num2str(sigma_gamma_psd*1e9) ' Ns/m'
 
 disp(['k: ' num2str(k_psd*1e6) '+-' num2str(sigma_k_psd*1e6) ' N/m'])
 
+end
+
+
+
+gamma=mean(gamma_psd*1e9)
+delta_gamma=std(gamma_psd*1e9)
+kappa=mean(k_psd*1e6)
+delta_kappa=std(k_psd*1e6)
+D=mean(Djj*1e12)
+delta_D=std(Djj*1e12)
 
