4266867367f31a7905af42176c724e02ccc31fb4
Laura Perez <laura.perez.garcia@physics.gu.se>
2020-03-10 20:58:37 +0100

    deleted all folders of calibration outside calibration folder

--- acf/f_acf_nl.m
+++
@@ -1,6 +0,0 @@
-function value = f_acf_nl(t, params)
-% The analytical function that is to be fitted to data, through
-% parameters in params, as function of t. Here we use a powerlaw
-
-%value = params(1)*power(t, params(2));
-value = params(1)*exp(-t/params(2));
